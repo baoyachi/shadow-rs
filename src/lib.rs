94c7a9d68fd172d937ea655ec4d325ad4c289689
baoyachi <liaoymxsdl@gmail.com>
2020-12-14 19:24:01 +0800

    fix issue:

--- src/lib.rs
+++ src/lib.rs
@@ -42,18 +42,21 @@
 //! # Quick Start
 //!
 //! ## step 1
-//! in your `cargo.toml` `packgae` with package add with below config
+//! In your `cargo.toml` `packgae` with package add with below config
 //!
 //! ```toml
 //! [package]
 //! build = "build.rs"
 //!
+//! [dependencies]
+//! shadow-rs = "0.4"
+//!
 //! [build-dependencies]
-//! shadow-rs = "0.3"
+//! shadow-rs = "0.4"
 //! ```
 //!
 //! ## step 2
-//! in your project add file `build.rs`,then add with below config
+//! In your project add file `build.rs`,then add with below config
 //!
 //! ```ignore
 //! use shadow_rs;
@@ -63,42 +66,45 @@
 //! ```
 //!
 //! ## step 3
-//! in your project find `bin` rust file.
-//! It's usually `main.rs`, you can find `[bin]` file with `Cargo.toml`,then add with below config
+//! In your project find `bin` rust file.It's usually `main.rs`, you can find `[bin]` file with `Cargo.toml`,then add with below config
+//! The `shadow!(build)` with `build` config,add Rust build mod in your project. Also you also use other name that you want configuration.
 //!
 //! ```ignore
-//! pub mod shadow{
-//!    include!(concat!(env!("OUT_DIR"), "/shadow.rs"));
-//! }
+//! #[macro_use]
+//! extern crate shadow_rs;
+//!
+//! shadow!(build);
 //! ```
 //!
 //! ## step 4
-//! then you can use const that's shadow build it(main.rs).
+//! Then you can use const that's shadow build it(main.rs).
+//!
+//! The `build` mod just we use `shadow!(build)` generated.
 //!
 //! ```ignore
 //! fn main(){
-//!    println!("{}",shadow::BRANCH); //master
-//!    println!("{}",shadow::SHORT_COMMIT);//8405e28e
-//!    println!("{}",shadow::COMMIT_HASH);//8405e28e64080a09525a6cf1b07c22fcaf71a5c5
-//!    println!("{}",shadow::COMMIT_DATE);//2020-08-16T06:22:24+00:00
-//!    println!("{}",shadow::COMMIT_AUTHOR);//baoyachi
-//!    println!("{}",shadow::COMMIT_EMAIL);//xxx@gmail.com
+//!    println!("{}",build::BRANCH); //master
+//!    println!("{}",build::SHORT_COMMIT);//8405e28e
+//!    println!("{}",build::COMMIT_HASH);//8405e28e64080a09525a6cf1b07c22fcaf71a5c5
+//!    println!("{}",build::COMMIT_DATE);//2020-08-16T06:22:24+00:00
+//!    println!("{}",build::COMMIT_AUTHOR);//baoyachi
+//!    println!("{}",build::COMMIT_EMAIL);//xxx@gmail.com
 //!
-//!    println!("{}",shadow::BUILD_OS);//macos-x86_64
-//!    println!("{}",shadow::RUST_VERSION);//rustc 1.45.0 (5c1f21c3b 2020-07-13)
-//!    println!("{}",shadow::RUST_CHANNEL);//stable-x86_64-apple-darwin (default)
-//!    println!("{}",shadow::CARGO_VERSION);//cargo 1.45.0 (744bd1fbb 2020-06-15)
-//!    println!("{}",shadow::PKG_VERSION);//0.3.13
-//!    println!("{}",shadow::CARGO_TREE); //like command:cargo tree
+//!    println!("{}",build::BUILD_OS);//macos-x86_64
+//!    println!("{}",build::RUST_VERSION);//rustc 1.45.0 (5c1f21c3b 2020-07-13)
+//!    println!("{}",build::RUST_CHANNEL);//stable-x86_64-apple-darwin (default)
+//!    println!("{}",build::CARGO_VERSION);//cargo 1.45.0 (744bd1fbb 2020-06-15)
+//!    println!("{}",build::PKG_VERSION);//0.3.13
+//!    println!("{}",build::CARGO_TREE); //like command:cargo tree
 //!
-//!    println!("{}",shadow::PROJECT_NAME);//shadow-rs
-//!    println!("{}",shadow::BUILD_TIME);//2020-08-16 14:50:25
-//!    println!("{}",shadow::BUILD_RUST_CHANNEL);//debug
+//!    println!("{}",build::PROJECT_NAME);//shadow-rs
+//!    println!("{}",build::BUILD_TIME);//2020-08-16 14:50:25
+//!    println!("{}",build::BUILD_RUST_CHANNEL);//debug
 //! }
 //!```
 //!
 //! ## Clap example
-//! And you can also use const with [clap](https://github.com/baoyachi/shadow-rs/blob/master/example_shadow/src/main.rs#L24_L26).
+//! And you can also use const with [clap](https://github.com/baoyachi/shadow-rs/blob/master/example_shadow/src/main.rs#L25_L27).
 //!
 //! For the user guide and futher documentation, please read
 //! [The shadow-rs document](https://github.com/baoyachi/shadow-rs).
@@ -128,6 +134,21 @@ use chrono::Local;
 pub use err::{SdResult, ShadowError};
 
 const SHADOW_RS: &str = "shadow.rs";
+
+/// Add a mod in project with `$build_mod`.
+///
+/// You can use `shadow!(build_shadow)`. Then shadow-rs can help you add a mod with name `build_shadow`.
+/// Next, use mod with name `build_shadow`,and also use const like:`build_shadow::BRANCH`.
+///
+/// Normally, you just config `shadow!(build)`.It looks more concise.
+#[macro_export]
+macro_rules! shadow {
+    ($build_mod:ident) => {
+        pub mod $build_mod {
+            include!(concat!(env!("OUT_DIR"), "/shadow.rs"));
+        }
+    };
+}
 
 #[derive(Debug)]
 pub struct Shadow {
