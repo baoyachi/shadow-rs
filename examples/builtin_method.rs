18420ee12fdda64824638ffaaa703b4d0ccca4a9
baoyachi <liaoymxsdl@gmail.com>
2021-01-14 11:23:55 +0800

    fix ci

--- examples/builtin_method.rs
+++ examples/builtin_method.rs
@@ -1,3 +1,4 @@
+// cargo run --example builtin_method
 fn main() {
     println!("debug:{}", shadow_rs::is_debug()); // check if this is a debug build. e.g 'true/false'
     println!("branch:{}", shadow_rs::branch()); // get current project branch. e.g 'master/develop'
