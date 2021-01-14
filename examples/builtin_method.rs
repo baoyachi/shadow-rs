147ed655ec4cb6e8d94ea0b81fb3f25aba2fd2b2
baoyachi <liaoymxsdl@gmail.com>
2021-01-15 00:06:38 +0800

    upgrade shadow-rs 0.5.12-pre

--- examples/builtin_method.rs
+++ examples/builtin_method.rs
@@ -2,4 +2,5 @@
 fn main() {
     println!("debug:{}", shadow_rs::is_debug()); // check if this is a debug build. e.g 'true/false'
     println!("branch:{}", shadow_rs::branch()); // get current project branch. e.g 'master/develop'
+    println!("tag:{}", shadow_rs::tag()); // get current project tag. e.g 'v1.3.5'
 }
