d408e179ec6f25182e1652913a501d920b393d0f
baoyachi <liaoymxsdl@gmail.com>
2021-08-11 21:32:11 +0800

    add doc

--- example_shadow_hook/src/main.rs
+++ example_shadow_hook/src/main.rs
@@ -3,6 +3,7 @@ use shadow_rs::shadow;
 shadow!(build);
 
 fn main() {
+    // how to use new_hook function: https://github.com/baoyachi/shadow-rs/blob/master/example_shadow_hook/build.rs
     println!("const:{}", build::HOOK_CONST); //expect:'hello hook const'
     println!("fn:{}", build::hook_fn()); //expect:'hello hook bar fn'
 }
