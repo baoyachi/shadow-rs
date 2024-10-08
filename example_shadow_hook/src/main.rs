9aeee8cb1e51d0273a10ed528704e04459b8fc18
baoyachi <liaoymxsdl@gmail.com>
2024-10-08 18:49:37 +0800

    fix:cargo clippy check

--- example_shadow_hook/src/main.rs
+++ example_shadow_hook/src/main.rs
@@ -6,4 +6,5 @@ fn main() {
     // how to use new_hook function: https://github.com/baoyachi/shadow-rs/blob/master/example_shadow_hook/build.rs
     println!("const:{}", build::HOOK_CONST); //expect:'hello hook const'
     println!("fn:{}", build::hook_fn()); //expect:'hello hook bar fn'
+    assert_eq!(build::hook_fn(), build::HOOK_CONST);
 }
