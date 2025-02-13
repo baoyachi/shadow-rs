8170acaf0d194291d542bada1eecbd2486a7c33b
baoyachi <liaoymxsdl@gmail.com>
2025-02-13 10:21:16 +0800

    complete wasm example

--- example_wasm/src/lib.rs
+++ example_wasm/src/lib.rs
@@ -1,1 +1,16 @@
+shadow!(shadow);
 
+use serde_json::json;
+use shadow_rs::shadow;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn version() -> JsValue {
+    let json = json!({
+        "branch/tag":if shadow::BRANCH.is_empty(){ shadow::BRANCH } else{ shadow::LAST_TAG },
+        "commit_hash":shadow::SHORT_COMMIT,
+        "build_time":shadow::BUILD_TIME,
+    });
+    let val = json.to_string();
+    serde_wasm_bindgen::to_value(&val).unwrap()
+}
